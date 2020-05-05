43af6108e2551af01288f0edb41f28fc96b94c76
Adam Hughes <ashughes07@aol.com>
2020-05-05 16:13:00 +0100

    inital commit

--- roll.py
+++ roll.py
@@ -1,9 +1,18 @@
 import discord
 import random
-import re
+import os
+from dotenv import load_dotenv, find_dotenv
 from discord.ext import commands
 
+random.seed()
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
+
 client = commands.Bot(command_prefix= "-")
+
+@client.event
+async def on_ready():
+    print("Bot is ready!")
 
 def random_dice(die_amount, die):
     get_roll = [random.randint(1, die) for _ in range(die_amount)]
@@ -16,10 +25,6 @@ def random_dice(die_amount, die):
 
     return total_roll
 
-@client.event
-async def on_ready():
-    print("Bot is ready!")
-
 @client.command()
 async def roll(ctx, user_post):
     die = 0
@@ -27,10 +32,13 @@ async def roll(ctx, user_post):
     get_roll = 0
     user_request = []
 
+    # Take characters from the command
     for char in user_post:
         user_request.append(char)
 
+    # Create a new list with only the integers e.g 1d4 = [1, 4]
     new_list = [num for num in user_request if num.isdigit()]
+    
     if len(new_list) > 3:
         die_amount = int(str(new_list[0]) + str(new_list[1]))
         die = int(str(new_list[2] + str(new_list[3])))
@@ -46,4 +54,4 @@ async def roll(ctx, user_post):
     await ctx.send(f'Rolling {die_amount}d{die}\nYou rolled: {get_roll}')
 
 
-client.run('')
+client.run(TOKEN)
